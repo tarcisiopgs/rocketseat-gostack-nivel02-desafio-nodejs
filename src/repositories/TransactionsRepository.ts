93b22aae30a3b3dd6dc1c4821b88ce72105ea37a
Tarcísio Pedro <tarcisiopgs@gmail.com>
2020-05-16 08:23:48 -0300

    repo update

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  value: number;
+  title: string;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const result: Balance = {
+      total: 0,
+      income: 0,
+      outcome: 0,
+    };
+
+    for (const { value, type } of this.transactions) {
+      result[type] += value;
+    }
+
+    result.total = result.income - result.outcome;
+
+    return result;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ value, type, title }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type not allowed');
+    }
+
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (value > total) {
+        throw Error('balance not available');
+      }
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
